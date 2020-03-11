9e5155966eeabb5d8032331c825eed388dcbbf48
Deepanshu Gupta <M1046873@mindtree.com>
2020-03-11 09:08:25 +0530

    card css added

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -5,6 +5,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import { connect } from 'react-redux';
 
 const useStyles = makeStyles({
   root: {
@@ -16,28 +17,57 @@ const useStyles = makeStyles({
   },
 });
 
-const ProductCard = () => {
+const ProductCard = (props) => {
   const classes = useStyles();
 
+  const renderCards = () => {
+    const data = props.productData;
+    const styleCard = {
+      margin: '10px',
+      width: '800px',
+    }
+    if (data.length > 0) {
+      return data.map((data) => {
+        return (
+          <Card className={classes.root} key={data.uniq_id} style={styleCard}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://images.yourstory.com/cs/wordpress/2016/08/125-fall-in-love.png"
+                title="ProductName"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  <b>Brand : </b>{data.brand}
+                </Typography>
+                <Typography gutterBottom variant="h5" component="h5">
+                  <b>Product Name :</b>{data.product_name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  <b>Description: </b>{data.description.substring(0, 100)}
+                </Typography>
+                <Typography component="h6">
+                  <b>Price </b>{data.discounted_price}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        );
+      });
+    }
+    return <p>No data</p>;
+  }
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://akm-img-a-in.tosshub.com/indiatoday/images/story/201908/PArle-770x433.png?2TCEl_5_2rZGuRummkQOkQbtJoMXfixx"
-          title="ProductName"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Product Name
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Product Description
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+      {renderCards()}
+    </div>
   );
 }
 
-export default ProductCard;
+const mapStateToProps = (state) => {
+  return {
+    productData: state.search,
+    login: state.login
+  }
+}
+export default connect(mapStateToProps, null)(ProductCard);
